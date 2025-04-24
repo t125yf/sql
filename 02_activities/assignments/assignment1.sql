746e70a75b32c2511594880bc42be83592ba92bc
t125yf <T125YF@GMAIL.COM>
2025-04-24 21:02:48 +0200

    Update assignment1.sql

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -135,7 +135,13 @@ When inserting the new vendor, you need to appropriately align the columns to be
 VALUES(col1,col2,col3,col4,col5) 
 */
 
+CREATE TEMP TABLE temp.new_vendor AS
+SELECT *
+FROM vendor;
 
+-- 2. Insert the new 10th vendor
+INSERT INTO temp.new_vendor (vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+VALUES (10, 'Thomass Superfood Store', 'Fresh Focused', 'Thomas', 'Rosenthal');
 
 -- Date
 /*1. Get the customer_id, month, and year (in separate columns) of every purchase in the customer_purchases table.
